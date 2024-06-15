33f6aaae3ae204fa6e6dc1829fd5cc12fb2c9c6a
Bruno13f <bruno13f@outlook.pt>
2024-06-15 15:50:30 +0100

    fix form name wizard service

--- ProjLTI/AppLTI/serviceNameForm.Designer.cs
+++ ProjLTI/AppLTI/serviceNameForm.Designer.cs
@@ -87,6 +87,7 @@
             this.Controls.Add(this.textBoxNome);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "serviceNameForm";
